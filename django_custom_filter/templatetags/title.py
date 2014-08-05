449ed4f6701b0bc498d5170b00dac9fe78da5eda
Alex Clark <aclark@aclark.net>
2014-08-05 17:51:24 -0400

    Update

--- django_custom_filter/templatetags/title.py
+++ django_custom_filter/templatetags/title.py
@@ -1,8 +1,12 @@
 from django import template
 
+
 register = template.Library()
 
+
 @register.filter(name='title')
-def title():
+def title(value):
     """
+    Uppercase first letter, via http://stackoverflow.com/a/13525843
     """
+    return value[0].upper() + value[1:]
