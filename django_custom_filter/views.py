090c44c8e652d898095b98b51feff845578361ab
Alex Clark <aclark@aclark.net>
2014-08-11 09:19:01 -0400

    Update

--- django_custom_filter/views.py
+++ django_custom_filter/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render_to_response
+import datetime
 
 
 def root(request):
-    return render_to_response('root.html')
+    now = datetime.datetime.now()
+    return render_to_response('root.html', {'now', now})
 
