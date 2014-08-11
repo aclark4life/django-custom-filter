18014f1c429bb0f4806a72c1b42fcb3f77c4bb4a
Alex Clark <aclark@aclark.net>
2014-08-11 09:12:10 -0400

    Restringify

--- django_custom_filter/templatetags/title.py
+++ django_custom_filter/templatetags/title.py
@@ -10,4 +10,4 @@ def custom_title(value):
     Uppercase first letter of each word leaving remaining characters
     unchanged.
     """
-    return [word[0].capitalize() + word[1:] for word in value.split()]
+    return ' '.join([word[0].capitalize() + word[1:] for word in value.split()])
